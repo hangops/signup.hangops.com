ced6aa31e5fa62f426850a47dea6323ff07724cc
Brandon Burton <brandon@inatree.org>
2016-10-14 09:47:26 -0700

    tweak title name

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'hangops-slack',
+  community: process.env.COMMUNITY_NAME || 'hangops.slack.com',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'hangops.slack.com',
   // access token of slack
